df21beb8398d8345e9ce7505ccf460f397b32cd6
MartkCz <antik494@gmail.com>
2017-06-08 20:35:16 +0200

    dropped authorizator

--- src/DI/UserExtension.php
+++ src/DI/UserExtension.php
@@ -6,7 +6,6 @@ namespace Thunbolt\User\DI;
 
 use Nette\DI\CompilerExtension;
 use Thunbolt\User\Authenticator;
-use Thunbolt\User\Authorizator;
 use Thunbolt\User\User;
 use Thunbolt\User\UserStorage;
 
@@ -14,12 +13,7 @@ class UserExtension extends CompilerExtension {
 
 	/** @var array */
 	public $defaults = [
-		'entity' => 'Model\User',
-		'authenticator' => Authenticator::class,
-		'authorizator' => [
-			'enable' => TRUE,
-			'class' => Authorizator::class
-		]
+		'authenticator' => Authenticator::class
 	];
 
 	public function loadConfiguration(): void {
@@ -28,9 +22,6 @@ class UserExtension extends CompilerExtension {
 
 		$builder->addDefinition($this->prefix('authenticator'))
 			->setClass($config['authenticator'], [$config['entity']]);
-
-		$builder->addDefinition($this->prefix('authorizator'))
-			->setClass($config['authorizator']['class'], [$config['authorizator']['enable']]);
 	}
 
 	public function beforeCompile(): void {
@@ -43,8 +34,7 @@ class UserExtension extends CompilerExtension {
 
 		$builder->getDefinition('user')
 			->setClass(User::class)
-			->setFactory(User::class)
-			->addSetup('setAuthenticator', [$this->prefix('@authenticator')]);
+			->setFactory(User::class);
 	}
 
 }
